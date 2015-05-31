672f5eb921993fc079ff3f7b55a8cc65c665aa94
nickbetsworth <nick.betsworth@gmail.com>
2015-06-01 00:01:21 +0100

    Completed challenge

---
+++ HalloweenParty/HalloweenParty.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+//https://www.hackerrank.com/challenges/halloween-party
+
+
+int main() {
+	int num_tests;
+	std::cin >> num_tests;
+
+
+	return 0;
+}
