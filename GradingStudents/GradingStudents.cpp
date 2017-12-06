da548bd4a7544b42279664e663f2579b3c1cd206
nickbetsworth <nick.betsworth@gmail.com>
2017-12-06 20:57:11 +0000

    Added missing change.

--- GradingStudents/GradingStudents.cpp
+++ GradingStudents/GradingStudents.cpp
@@ -17,7 +17,7 @@ int main()
 		std::cin >> grade;
 
 		if (grade < 38)		std::cout << grade << std::endl;
-		else				std::cout << ((grade % 5) < 2 ? (grade + grade % 5) : grade) << std::endl;
+		else				std::cout << ((5 - (grade % 5)) < 3 ? (grade + (5 - grade % 5)) : grade) << std::endl;
 	}
 
 	return 0;
